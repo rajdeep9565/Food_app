a597ac32c0bb87ca765b5dc15fff4b67ca5bce14
RAJDEEP KUMAR SINGH <rajdeeprajsingh@gmail.com>
2021-11-12 13:07:42 +0530

    Asssignment_1

---
+++ component/navbar.js
@@ -0,0 +1,18 @@
+function navbar(){
+    return  `
+    <div id="home">
+    <h1> <a href="food.html">Home</a></h1>
+
+<div id="option">
+    <p> <a href="reciepe_day.html">Recipe of the day</a></p>
+    <p> <a href="search.html">Search</a></p>
+   
+    <p> <a href="latest.html">Latest Recipe</a></p> 
+
+</div>
+
+</div>
+`
+}
+
+    export default navbar
